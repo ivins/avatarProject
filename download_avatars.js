89bd2b61ec8b0932de2200f4fa3ade385ebe2464
silentscribe <seelive.i@gmail.com>
2019-01-15 05:50:41 +0000

    comments added

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,10 @@
 const request = require('request');
 const fs = require('fs');
-const token = require('./secrets.js');
+const token = require('./secrets.js'); // github token location
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// gets the contributors list and performs callback
 function getRepoContributors (repoOwner, repoName, cb) {
   const options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
@@ -18,8 +19,13 @@ function getRepoContributors (repoOwner, repoName, cb) {
     });
 }
 
+// calls the function with inputs from the terminal then parses the result
+// then for each user calls the downloadImageByURL function which
+// first checks if the avatars folder exists and creates it if it doesn't
+// then it requests the avatar and user and downloads the file saving it with
+// the user name as the file name.
 getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
-  console.log("Errors:", err);
+  console.log('Errors:', err);
   const parsedBody = JSON.parse(result);
   parsedBody.forEach(user => downloadImageByURL(user.avatar_url, user.login));
 });
