ba6a37add9763c5d224ebdd92176742bd8f91c8e
silentscribe <seelive.i@gmail.com>
2019-01-15 05:43:51 +0000

    code cleaned/delinted

--- download_avatars.js
+++ download_avatars.js
@@ -4,7 +4,6 @@ const token = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-//
 function getRepoContributors (repoOwner, repoName, cb) {
   const options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
@@ -22,25 +21,22 @@ function getRepoContributors (repoOwner, repoName, cb) {
 getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
   console.log("Errors:", err);
   const parsedBody = JSON.parse(result);
-  parsedBody.forEach(user => {
-    downloadImageByURL(user.avatar_url, user.login);
-  });
+  parsedBody.forEach(user => downloadImageByURL(user.avatar_url, user.login));
 });
 
-
-function downloadImageByURL(url, filePath) {
+function downloadImageByURL (url, filePath) {
   const mkdir = './avatars/';
-  if (!fs.existsSync(mkdir)){
+  if (!fs.existsSync(mkdir)) {
     fs.mkdirSync(mkdir);
   }
   request.get(url)
-       .on('error', function (err) {
-         throw err;
-       })
-       .on('response', function (response) {
-         console.log('Response Status Code: ', response.statusCode);
-         console.log('Response Status Message: ', response.statusMessage);
-         console.log('Content Type: ', response.headers['content-type']);
-       })
-       .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpeg'));
+    .on('error', function (err) {
+      throw err;
+    })
+    .on('response', function (response) {
+      console.log('Response Status Code: ', response.statusCode);
+      console.log('Response Status Message: ', response.statusMessage);
+      console.log('Content Type: ', response.headers['content-type']);
+    })
+    .pipe(fs.createWriteStream(`./avatars/${filePath}.jpeg`));
 }
