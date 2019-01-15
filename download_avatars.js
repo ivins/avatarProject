d6aa6815b84c938b996a4bb3f53b7810e84cd6e7
silentscribe <seelive.i@gmail.com>
2019-01-15 03:25:03 +0000

    get contr sending request. No auth yet.

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,26 @@
 const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  const url = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+  request(url, function(err, res, body) {
+    cb(err, body);
+    });
+}
+
+// request.get('https://sytantris.github.io/http-examples/future.jpg')
+//        .on('error', function (err) {
+//          throw err;
+//        })
+//        .on('response', function (response) {
+//          console.log('Response Status Code: ', response.statusCode);
+//          console.log('Response Status Message: ', response.statusMessage);
+//          console.log('Content Type: ', response.headers['content-type']);
+//        })
+//        .pipe(fs.createWriteStream('./future.jpg'));
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
